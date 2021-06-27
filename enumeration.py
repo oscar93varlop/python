6596f9a407d4040b637f303ab34c981dbcca13bf
oscar93varlop <39097708+oscar93varlop@users.noreply.github.com>
2021-06-27 12:23:23 -0500

    Add files via upload

---
+++ enumeration.py
@@ -0,0 +1,8 @@
+objetivo = int(input('escoge un numero'))
+respuesta = 0 
+while respuesta**2 < objetivo:
+    respuesta +=1
+if respuesta**2 == objetivo:
+    print(f'la raiz cuadrada de {objetivo} es {respuesta}')
+else:
+    print(f'{objetivo} no tiene una raiz cuadrada exacta')
